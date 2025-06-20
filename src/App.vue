2817ee011cb3cc1a0d1efc57e14daccfae069d7d
MrSteinfeger <bao66@hotmail.com>
2025-06-20 12:23:26 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
   <div>
-    <h1>Baos wundervolle Computer</h1>
-    <ItemList :items="computerListe" />
+    <h1>Daten vom Backend:</h1>
+    <p v-if="data">{{ data.name }} ist {{ data.age }} Jahre alt</p>
+    <p v-else>Lade Daten...</p>
   </div>
 </template>
 
 <script>
-import ItemList from "./components/ItemList.vue";
-
 export default {
-  name: "App",
-  components: {
-    ItemList,
-  },
   data() {
     return {
-      computerListe: ["Computer 1", "Computer 2", "Computer 3"],
-    };
+      data: null
+    }
   },
-};
-</script>
-
-<style scoped>
-h1 {
-  font-family: Arial, sans-serif;
-  color: #2c3e50;
-  text-align: center;
+  mounted() {
+    fetch("https://baobaowebtech.onrender.com") 
+      .then(res => res.json())
+      .then(data => this.data = data)
+  }
 }
-</style>
+</script>
